4e7eac097468f8539ff10f86cbb1479bb3a9531c
Carmepiola22 <carmelotolosa01@gmail.com>
2024-05-02 21:52:58 -0300

    ultimo

--- validar.php
+++ validar.php
@@ -1,8 +1,15 @@
 <?php
-$Name="Choco";
-$Password="1234";
+$Name="";
+$Password="";
+
+if(isset($_POST['ingresarR'])){
+    $Name=($_POST['ingresarR']);
+    $Password=($_POST[])
+}
+
+
 if (isset($_POST['ingresar'])) {  
-    if (($_POST['nombre']== $Name)&& ($_POST['pass']== $Password))
+    if (($_POST['nombre']== $Name) && ($_POST['pass']== $Password))
     echo"<h1>Has podido entrar</h1>";
 else{
 echo "no te hemos permitido acceder, pues no eres el admin";
